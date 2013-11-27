52528c689f2fb1fb6b7e0ae271081831fb54b54e
Phillip Johnsen <johphi@gmail.com>
2013-11-27 21:52:53 +0100

    Example of creating URLs in the HomeController

--- src/main/java/no/finntech/example/controller/HomeController.java
+++ src/main/java/no/finntech/example/controller/HomeController.java
@@ -1,21 +1,20 @@
 package no.finntech.example.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping(value="/")
 public final class HomeController {
 
     @RequestMapping(method = RequestMethod.GET)
-	public ModelAndView get() throws IOException{
-		return new ModelAndView("page.home");
+	public ModelAndView get(final UriComponentsBuilder builder) throws IOException{
+        return new ModelAndView("page.home", "urlToRandomPage", builder.path("/random").build().toString());
 	}
 
 }
--- src/main/webapp/WEB-INF/tiles/pages/home/home_body.jsp
+++ src/main/webapp/WEB-INF/tiles/pages/home/home_body.jsp
@@ -1,4 +1,4 @@
 <div>
     <p>This is the homepage.</p>
-    <p>Visit the <a href="/random">random generator page</a> for some magic</p>
+    <p>Visit the <a href="${urlToRandomPage}">random generator page</a> for some magic</p>
 </div>
